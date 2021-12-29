3a34098b35b9ad79f8bbb5c5c126ae979528a2af
walshm36 <81037131+walshm36@users.noreply.github.com>
2021-12-29 13:49:12 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,9 @@ config = {
     "alpha_vantage": {
         "key": "J4JLQKLV6IR0Q0HG", # Claim your free API key here: https://www.alphavantage.co/support/#api-key
         "symbol": "VOD.LON",
-        "outputsize": "full",
-        "key_adjusted_close": "5. adjusted close",
+        "outputsize": "compact",
+        "output_format": "pandas",
+        "close": "4. close",
     },
     "data": {
         "window_size": 20,
@@ -46,4 +47,33 @@ config = {
 }
 
 
+def download_data(config):
+    ts = TimeSeries(key=config["alpha_vantage"]["key"])
+    data, meta_data = ts.get_daily(config["alpha_vantage"]["symbol"], outputsize=config["alpha_vantage"]["outputsize"])
 
+    data_date = [date for date in data.keys()]
+    data_date.reverse()
+
+    data_close_price = [float(data[date][config["alpha_vantage"]["close"]]) for date in data.keys()]
+    data_close_price.reverse()
+    data_close_price = np.array(data_close_price)
+
+    num_data_points = len(data_date)
+    display_date_range = "from " + data_date[0] + " to " + data_date[num_data_points-1]
+    print("Number data points", num_data_points, display_date_range)
+
+    return data_date, data_close_price, num_data_points, display_date_range
+
+data_date, data_close_price, num_data_points, display_date_range = download_data(config)
+
+# plot
+
+fig = figure(figsize=(25, 5), dpi=80)
+fig.patch.set_facecolor((1.0, 1.0, 1.0))
+plt.plot(data_date, data_close_price, color=config["plots"]["color_actual"])
+xticks = [data_date[i] if ((i%config["plots"]["xticks_interval"]==0 and (num_data_points-i) > config["plots"]["xticks_interval"]) or i==num_data_points-1) else None for i in range(num_data_points)] # make x ticks nice
+x = np.arange(0,len(xticks))
+plt.xticks(x, xticks, rotation='vertical')
+plt.title("Daily close price for " + config["alpha_vantage"]["symbol"] + ", " + display_date_range)
+plt.grid(b=None, which='major', axis='y', linestyle='--')
+plt.show()
