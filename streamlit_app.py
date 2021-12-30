7988bd6713eb6fb09aa39a975e8c5e505a339f87
walshm36 <81037131+walshm36@users.noreply.github.com>
2021-12-30 12:50:24 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,16 +65,17 @@ def download_data(config):
     return data_date, data_close_price, num_data_points, display_date_range
 
 data_date, data_close_price, num_data_points, display_date_range = download_data(config)
+print(data_close_price)
 
 # plot
 
-fig = figure(figsize=(25, 5), dpi=80)
-fig.patch.set_facecolor((1.0, 1.0, 1.0))
-plt.plot(data_date, data_close_price, color=config["plots"]["color_actual"])
-xticks = [data_date[i] if ((i%config["plots"]["xticks_interval"]==0 and (num_data_points-i) > config["plots"]["xticks_interval"]) or i==num_data_points-1) else None for i in range(num_data_points)] # make x ticks nice
-x = np.arange(0,len(xticks))
-plt.xticks(x, xticks, rotation='vertical')
-plt.title("Daily close price for " + config["alpha_vantage"]["symbol"] + ", " + display_date_range)
-plt.grid(b=None, which='major', axis='y', linestyle='--')
-plt.set_loglevel('WARNING')
-plt.show()
+#fig = figure(figsize=(25, 5), dpi=80)
+#fig.patch.set_facecolor((1.0, 1.0, 1.0))
+##plt.plot(data_date, data_close_price, color=config["plots"]["color_actual"])
+#xticks = [data_date[i] if ((i%config["plots"]["xticks_interval"]==0 and (num_data_points-i) > config["plots"]["xticks_interval"]) or i==num_data_points-1) else None for i in range(num_data_points)] # make x ticks nice
+#x = np.arange(0,len(xticks))
+#plt.xticks(x, xticks, rotation='vertical')
+#plt.title("Daily close price for " + config["alpha_vantage"]["symbol"] + ", " + display_date_range)
+#plt.grid(b=None, which='major', axis='y', linestyle='--')
+#plt.set_loglevel('WARNING')
+#plt.show()
